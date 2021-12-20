95fc9e88e28cd2e3053b4224ca08c42323633e4e
Hugefiver <i@iruri.moe>
2021-12-20 16:40:59 +0800

    update test source

--- flags_test.go
+++ flags_test.go
@@ -107,8 +107,8 @@ func TestGetArg(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			os.Args = tt.osArgs
-			if got, _ := GetArg(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetArg() = %v, want %v", got, tt.want)
+			if got, _ := GetArg(); !reflect.DeepEqual(*got, tt.want) {
+				t.Errorf("GetArg() = %v, want %v", *got, tt.want)
 			}
 		})
 	}
