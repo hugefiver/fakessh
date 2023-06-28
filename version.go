49b45a2af634cc1ed3070fa964020e544db2532e
Hugefiver <i@iruri.moe>
2023-06-28 05:15:46 +0000

    update version.go

--- version.go
+++ version.go
@@ -6,17 +6,18 @@ import (
 	"github.com/hugefiver/fakessh/conf"
 )
 
-var version = "0.3.1"
+var version = "0.3.x-master"
 var commitId = "unknown"
 var buildTime = "unknown"
 var goversion = "unknown"
 
 func showVersion() {
 	fmt.Printf(`FakeSSH - a fake SSH server
-	
+
 version: %s
 commit: %s
 build time: %s
 go version: %s
-SSH version: %s`, version, commitId, buildTime, goversion, conf.DefaultSSHVersion)
+Default SSH version: %s
+`, version, commitId, buildTime, goversion, conf.DefaultSSHVersion)
 }
