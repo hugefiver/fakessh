53ddc741b1c6f2d6ac8933ae4ca7103881ac4d1f
Hugefiver <i@iruri.moe>
2025-01-14 18:09:52 +0800

    nix: refactor

--- default.nix
+++ default.nix
@@ -3,35 +3,41 @@
   go,
   buildGoModule,
   fetchFromGitHub,
-}:
-buildGoModule rec {
-  pname = "fakessh";
-  version = "0.5.1";
-  commitId = "19b59ef";
+}: let
+  info = rec {
+    pname = "fakessh";
+    version = "0.5.1";
+    commitId = "19b59ef";
 
-  src = fetchFromGitHub {
-    owner = "hugefiver";
-    repo = "fakessh";
-    rev = "v${version}";
-    hash = "sha256-3zCuRuu1HWRotOjPZNqhmBneObmCuVgdTvphS5Fn4nU=";
+    src = fetchFromGitHub {
+      owner = "hugefiver";
+      repo = "fakessh";
+      rev = "v${version}";
+      hash = "sha256-3zCuRuu1HWRotOjPZNqhmBneObmCuVgdTvphS5Fn4nU=";
+    };
   };
+in
+  buildGoModule rec {
+    inherit (info) pname version commitId src;
 
-  vendorHash = "sha256-pgW9WNPvyANFTPTJYDd83zZnx23y9LqYZ3ZYk+BzVHI=";
+    vendorHash = "sha256-pgW9WNPvyANFTPTJYDd83zZnx23y9LqYZ3ZYk+BzVHI=";
 
-  ldflags = [
-    "-s"
-    "-w"
-    "-X=main.version=${version}"
-    "-X=main.goversion=${go.version}"
-    # "-X=main.buildTime=${envBuildTime}"
-    "-X=main.commitId=${commitId}"
-  ];
+    ldflags = [
+      "-s"
+      "-w"
+      "-X=main.version=${version}"
+      "-X=main.goversion=${go.version}"
+      # "-X=main.buildTime=${envBuildTime}"
+      "-X=main.commitId=${commitId}"
+    ];
 
-  meta = {
-    description = "Fake SSH Server | 一个假的 SSH Server";
-    homepage = "https://github.com/hugefiver/fakessh";
-    license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ hugefiver ];
-    mainProgram = "fakessh";
-  };
-}
+    subPackages = [ "." ];
+
+    meta = {
+      description = "Fake SSH Server | 一个假的 SSH Server";
+      homepage = "https://github.com/hugefiver/fakessh";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [hugefiver];
+      mainProgram = "fakessh";
+    };
+  }
