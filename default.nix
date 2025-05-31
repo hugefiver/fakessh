da7c37c3f1eb2b9bf78f0d4525f5926dc6c17dd5
Hugefiver <i@iruri.moe>
2025-05-31 12:38:04 +0000

    nix: add default.nix

--- default.nix
+++ default.nix
@@ -1,43 +1,88 @@
-{
-  lib,
-  go,
-  buildGoModule,
-  fetchFromGitHub,
-}: let
-  info = rec {
+{ lib, stdenv, buildGoModule, fetchFromGitHub, fetchurl, pkgMetadata, go }:
+
+let
+  inherit (pkgMetadata) version commitId srcHash vendorHash releaseAssets;
+  getReleaseAssetForSystem = system:
+    lib.findFirst (asset:
+      let
+        nixOs = if stdenv.isLinux then
+          "linux"
+        else if stdenv.isDarwin then
+          "darwin"
+          # else if stdenv.isWindows
+          # then "windows"
+        else
+          null;
+
+        nixArch = if stdenv.hostPlatform.isx86_64 then
+          "amd64" # x86_64 -> amd64
+        else if stdenv.hostPlatform.isAarch64 then
+          "arm64" # aarch64 -> arm64
+        else
+          null;
+
+        matchesOs = nixOs != null && lib.stringContains asset.name nixOs;
+        matchesArch = nixArch != null && lib.stringContains asset.name nixArch;
+      in matchesOs && matchesArch) null releaseAssets;
+
+  finalSelectedReleaseAsset = getReleaseAssetForSystem stdenv.system;
+
+  meta = {
+    description = "Fake SSH Server | 一个假的 SSH Server";
+    homepage = "https://github.com/hugefiver/fakessh";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ hugefiver ];
+    mainProgram = "fakessh";
+  };
+in rec {
+
+  default = fakessh;
+
+  fakessh = buildGoModule {
     pname = "fakessh";
-    version = "0.5.1";
-    commitId = "19b59ef";
+    inherit version meta;
 
     src = fetchFromGitHub {
       owner = "hugefiver";
       repo = "fakessh";
-      rev = "v${version}";
-      hash = "sha256-3zCuRuu1HWRotOjPZNqhmBneObmCuVgdTvphS5Fn4nU=";
+      rev = "${version}";
+      sha256 = srcHash;
     };
-  };
-in
-  buildGoModule rec {
-    inherit (info) pname version commitId src;
 
-    vendorHash = "sha256-pgW9WNPvyANFTPTJYDd83zZnx23y9LqYZ3ZYk+BzVHI=";
+    vendorHash = vendorHash;
+
+    subPackages = [ "." ];
 
     ldflags = [
       "-s"
       "-w"
       "-X=main.version=${version}"
       "-X=main.goversion=${go.version}"
-      # "-X=main.buildTime=${envBuildTime}"
       "-X=main.commitId=${commitId}"
     ];
 
-    subPackages = [ "." ];
+  };
 
-    meta = {
-      description = "Fake SSH Server | 一个假的 SSH Server";
-      homepage = "https://github.com/hugefiver/fakessh";
-      license = lib.licenses.mit;
-      maintainers = with lib.maintainers; [hugefiver];
-      mainProgram = "fakessh";
-    };
-  }
+  # fakessh-bin = stdenv.mkDerivation {
+  #   pname = "fakessh-bin";
+  #   inherit version meta;
+
+  #   src =
+  #     if finalSelectedReleaseAsset != null
+  #     then
+  #       fetchurl {
+  #         url = finalSelectedReleaseAsset.url;
+  #         sha256 = finalSelectedReleaseAsset.sha256;
+  #       }
+  #     else
+  #       throw ''
+  #         No suitable release asset found for system ${stdenv.system} in nix-metadata.json. 
+  #         Please ensure the asset exists and the naming convention is handled in getReleaseAssetForSystem function.'';
+
+  #   installPhase = ''
+  #     mkdir -p $out/bin
+  #     mv $src/fakessh $out/bin/fakessh
+  #     chmod +x $out/bin/fakessh
+  #   '';
+  # };
+}
